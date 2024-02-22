ef9835d2ca5a42b956ccf8154ac568aee75180d6
Krisha Maree <8805@holbertonstudents.com>
2024-02-22 01:01:02 +0000

    A C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,9 +4,10 @@
  * main - Entry point
  *
  * Return: Always 0 (Sucess)
- * */
+ * /
 
-int main(void) {
+int main(void)
+{
 	printf(with proper grammar, but the outcome is a piece of art,\n);
 	return 0;
 }

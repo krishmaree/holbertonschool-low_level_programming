c6cc8b1069119d5e9c3c676c6ee3e8373a46c4a2
krisha Maree <8805@holbertonstudents.com>
2024-02-23 06:45:44 +0000

    prints the alphabet in lowercas, excpet for q and e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -11,12 +11,13 @@ int main(void)
 	char c;
 
 	for (c = 'a' ; c <= 'z' ; c++)
-{
+	{	
 		if (c != 'q' && c != 'e')
-	{
-	putchar (c);
+		{
+			putchar (c);
+		}	
 	}
-}
+
 	putchar ('\n');
 
 	return (0);

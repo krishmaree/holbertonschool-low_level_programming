4ebeb5397668eee22e2162de166c6f64ecbecd1f
Krisha maree <8805@holbertonstudents.com>
2024-02-24 05:52:39 +0000

    Print all prossible single digits separated by comma and space

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single digit numbers
+ * separated by comma and space
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int c;
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		putchar(c);
+
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}

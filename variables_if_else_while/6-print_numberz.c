1a42dafa6f0b9f80afa702cba104cbe129c1cd41
Krisha maree <8805@holbertonstudents.com>
2024-02-24 02:26:52 +0000

    Print single digits from 0-9 using int

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/** 
+ * main - Prints all single digit numbers from 0 to 10
+ * without using char
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+	{
+		int c;
+
+		for(c = '0' ; c <= '9' ; c++)
+			putchar(c);
+
+		putchar('\n');
+		return (0);
+	}

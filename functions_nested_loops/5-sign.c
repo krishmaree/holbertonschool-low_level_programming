81eacfee426e45dd9a0658f4daf32d73443b683c
Krisha Maree <8805@holbertonstudents.com>
2024-03-03 08:02:49 +0000

    prints the sign of a number

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _print_sign - Prints the sign of a number
+ * print_sign - Prints the sign of a number
  * @n: The character to check
  *
- * Return 1 and print + if n is greater than zero,
+ * Return: 1 and print + if n is greater than zero,
  * 0 and prints 0 if n is zero,
  * -1 and prints - if n is less than zero
  */

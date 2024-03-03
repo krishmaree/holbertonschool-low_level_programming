61c127e747361bc34d285316ba7c7fc7b2487926
Krisha Maree <8805@holbertonstudents.com>
2024-03-03 06:54:27 +0000

    Prints out each character of _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** main - Program that prints _putchar followed by a new line
+/**
+ * main - Program that prints _putchar followed by a new line
  *
  * Return - Always 0 (Success)
  *

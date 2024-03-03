ebc6a31838564ad8409946c19b48dbbc54feca78
Krisha Maree <8805@holbertonstudents.com>
2024-03-03 09:07:20 +0000

    prints the 9 times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,23 +9,22 @@ void times_table(void)
 
 	for (row = 0; row <= 9; row++)
 	{
-		_putchar('0');
-
 		for (column = 0; column <= 9; column++)
 		{
 			result = row * column;
-
-			_putchar(',');
-			_putchar(' ');
-			if (result < 10)
+			if (column == 0)
 			{
-				_putchar(' ');
-				_putchar(result + 0);
+				_putchar('0' + result);
 			}
 			else
 			{
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+			if (result < 10)
+				_putchar(' ');
+			else
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
 			}
 		}
 

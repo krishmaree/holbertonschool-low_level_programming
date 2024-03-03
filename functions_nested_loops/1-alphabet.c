373afe8d5c61ff09899ce77ad0cee2c052519efc
Krisha Maree <8805@holbertonstudents.com>
2024-03-03 07:08:48 +0000

    Prints the alphabet in lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Prints the alphabet in lowercase followed by a new line
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+}

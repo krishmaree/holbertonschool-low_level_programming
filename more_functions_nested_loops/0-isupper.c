410a2b2c733f3bd9e07f6ccf81ef0ed9c1dab85a
Krisha Maree <8805@holbertonstudents.com>
2024-03-04 02:53:46 +0000

    Checks for uppercase characters

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * isupper: Check for uppercase character
+ * Returns 1 if c is uppercase
+ * Returns 0 otherwise
+ *
+ * @c: is the character to be checked
+ */
+
+int _isupper(int c)
+{
+       if (c >= 'A' && c <= 'Z')
+	       return (1);
+       else
+	       return (0);
+}
---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+C project- More functions, more nested loops

b8ee5b987e7a141f462eac2d347fbfd5cac34e02
Krisha Maree <8805@holbertonstudents.com>
2024-03-04 04:09:20 +0000

    Print the numbers from 0-9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - Prints the numbers from 0-9
+ * followed by a new line
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0' ; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}

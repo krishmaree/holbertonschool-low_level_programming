b824cc17b95236e1cc87aed056eff9937de1ee6b
‘Krisha <8805@holbertonstudents.com>
2024-03-09 08:14:34 +0000

    Function that capitalizes all words of a string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,22 +8,24 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0;
+	int capital = 1;
 
-	if (str[i] >= 'a' && str[i] <= 'z')
+	for (int i = 0; str[i] != '\0'; i++)
 	{
-		str[i] = str[i] - ('a' - 'A');
-	}
-	while (str[i] != '\0')
-	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		if ((str[i] >= 'a' && str[i] <= 'z') && capital)
 		{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-			{
-				str[i + 1] = str[i + 1] - ('a' - 'A');
-			}
+			str[i] -= ('a' - 'A');
+			capital = 0;
 		}
-		i++;
+		else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+			str[i] == ',' || str[i] == ';' ||
+			str[i] == '.' || str[i] == '!' ||
+			str[i] == '?' || str[i] == '"' ||
+			str[i] == '(' || str[i] == ')' ||
+			str[i] == '{' || str[i] == '}')
+		{
+			capital = 1;
+		}
 	}
 	return (str);
 }

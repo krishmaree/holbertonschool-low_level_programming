08b726e5ecef765789a2b768d28e0de111064b6e
‘Krisha <8805@holbertonstudents.com>
2024-03-09 08:44:15 +0000

    Capitalizes words of a string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,30 +8,21 @@
  */
 char *cap_string(char *str)
 {
-	int capital = 1;
 	int i;
+	int j = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((str[i] >= 'a' && str[i] <= 'z') && capital)
-		{
-			str[i] -= ('a' - 'A');
-			capital = 0;
-		}
-		else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-			str[i] == ',' || str[i] == ';' ||
-			str[i] == '.' || str[i] == '!' ||
-			str[i] == '?' || str[i] == '"' ||
-			str[i] == '(' || str[i] == ')' ||
-			str[i] == '{' || str[i] == '}')
-		{
-			capital = 1;
-		}
-		else if (!((str[i] >= 'a' && str[i] <= 'z') ||
-			(str[i] >= 'A' && str[i] <= 'Z')))
-		{
-			capital = 1;
-		}
+		if (str[i] >= 97  && str[i] <= 122 && j == 0)
+	{
+		str[i] -= 32;
+		j = 1;
+	}
+	else if (((str[i] >= 65 && str[i] <= 90)
+	|| (str[i] >= 48 && str[i] <= 57)) && j == 0)
+		j = 1;
+	if (str[i] == 32 || str[i] == 10 || str[i] == 46 || str[i] == 9)
+		j = 0;
 	}
 	return (str);
 }

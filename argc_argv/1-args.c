f11a003a831ad83f0e20ea554a22e1f329cd9923
‘Krisha <8805@holbertonstudents.com>
2024-03-13 00:31:31 +0000

    Program that prints the number of arguments passed into it

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Program that prints the number of arguments passed into it
+ * @argc: number of command line arguments
+ * @argv: an array contatining the command line arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("argc = %d/n", argc);
+	return (0);
+}

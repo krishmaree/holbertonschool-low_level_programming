28e4cefd44a98b900e3590525e7bbc78c56b734e
‘Krisha <8805@holbertonstudents.com>
2024-03-19 10:43:17 +0000

    Concatenates two strings using string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,9 +12,8 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len_s1 = 0, len_s2 = 0;
-	char *concatenated;
-	unsigned int i, j;
+	unsigned int len_s1 = 0, len_s2 = 0, total_length, i, j;
+	char *result;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -22,24 +21,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[len_s1] != '\0')
+	while (s1[len_s1])
 		len_s1++;
 
-	while (s2[len_s2] != '\0')
+	while (s2[len_s2])
 		len_s2++;
 
-	if (n >= len_s2)
-		n = len_s2;
+	total_length = len_s1 + n;
 
-	concatenated = malloc(sizeof(char) * (len_s1 + n + 1));
-	if (concatenated == NULL)
+	result = malloc(sizeof(char) * (total_length + 1));
+	if (result == NULL)
 		return (NULL);
 
 	for (i = 0; i < len_s1; i++)
-		concatenated[i] = s1[i];
+		result[i] = s1[i];
 
-	for (j = 0; j < n; j++)
-		concatenated[i + j] = s2[j];
+	for (j = 0; j < n && s2[j] != '\0'; j++)
+		result[len_s1 + j] = s2[j];
 
-	return (concatenated);
+	result[total_length] = '\0';
+
+	return (result);
 }

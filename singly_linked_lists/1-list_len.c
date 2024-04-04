ae6d38ffc739138343ee956e4a6e599c2fde2db1
‘Krisha <8805@holbertonstudents.com>
2024-04-04 04:54:00 +0000

    Function that return the number of nodes in a linked list

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * size_t list_len - returns the number of elements in a linked list_t list
+ * list_len - returns the number of elements in a linked list_t list
  * @h: pointer to the head of the list
  *
  * Return: the number of nodes in the list

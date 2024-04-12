5aba55167a11f17d04d99f7c0482dfdae5f4abc3
‘Krisha <8805@holbertonstudents.com>
2024-04-12 12:41:35 +0000

    Creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,7 +11,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd,i = 0, bytes_written;
+	int fd, i = 0, bytes_written;
 
 	if (filename == NULL)
 		return (-1);
@@ -23,15 +23,19 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1); /* if open fails, return -1*/
 
-	while (text_content[i] != '\0')
-		i++;
-	{ /* use strlen to determine length of string*/
-		bytes_written = write(fd, text_content, i);
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+			i++;
+	}
+
+	 /* use strlen to determine length of string*/
+
+	bytes_written = write(fd, text_content, i);
+	if (bytes_written == -1)
+	{
+		close(fd);
+		return (-1);
 	}
 	close(fd);
 	return (-1);

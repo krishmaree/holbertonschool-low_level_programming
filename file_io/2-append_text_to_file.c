c18805829b9d14f0e442d57cb606e7279d077e7e
‘Krisha <8805@holbertonstudents.com>
2024-04-14 06:30:05 +0000

    Function that appends a text to the end of a file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -5,7 +5,7 @@
  * @filename: name of the file
  * @text_content: NULL terminated string to add at the end of the file
  *
- * Retunr: 1 on success, and -1 on failure
+ * Return: 1 on success, and -1 on failure
  *
  */
 int append_text_to_file(const char *filename, char *text_content)

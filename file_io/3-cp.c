90118421e72d296625086e5bd05d536fcf23918b
‘Krisha <8805@holbertonstudents.com>
2024-04-14 13:27:59 +0000

    Copies content of one file to another

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,7 +14,7 @@ int main(int ac, char *av[])
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd_from = open(av[1], O_RDONLY);/*opens source file for reading*/
